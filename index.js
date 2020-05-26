e7352f602b729e1e0778990c0dcf8686f3e4e3e7
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-05-26 16:37:25 -0700

    updated form

---
+++ index.js
@@ -0,0 +1,16 @@
+// const button = document.querySelector('.gradient-button');
+
+// function validateEmail() {
+//   var emailText = document.getElementById('email').value;
+//   var pattern = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
+//   if (pattern.test(emailText)) {
+//     return true;
+//   } else {
+//     alert('bad');
+//     return false;
+//   }
+// }
+
+// window.onload = function () {
+//   document.getElementById('form').onmouseover = validateEmail;
+// };
